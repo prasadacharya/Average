63527e41e754e285f13ac54f20a81804287508cf
prasadacharya <prasada@mail.usf.edu>
2020-01-22 20:07:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// C# program to accept user inputs and calculate
+
+using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide your input for a");
+            string A = Console.ReadLine();
+            int a = Convert.ToInt32(A);
+
+            Console.WriteLine("Please provide your input for b");
+            string B = Console.ReadLine();
+            int b = Convert.ToInt32(B);
+
+            Console.WriteLine("Please provide your input for c");
+            string C = Console.ReadLine();
+            int c = Convert.ToInt32(C);
+
+            int s = (a + b + c) / 3;
+
+            Console.WriteLine("s value is" + s);
+        }
+    }
+}
+
+
